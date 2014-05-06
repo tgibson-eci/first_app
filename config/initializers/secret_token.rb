7335a2687cc5bf2bf7d0041c83982d905bbf3071
Todd Gibson <tgibson@ecipay.com>
2014-05-06 10:09:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4fae984d1d5edf35918920f0feb5b72a1b763620c5cb4e991ad3475b077d7c286ef71a2ccaa4e4e37c2d04bc6094881d96736bfb581715775276df0e502d180c'
